97b87bf726511b2800abe8f016c5aab3bacb0631
Luciano <lucianobarrerapaez@gmail.com>
2021-09-22 03:01:58 +0200

    HE CONSEGUIDO AÑADIR USUARIOS EMAIL PASSWORD EXITOSAMENTE A FIREBASE, SOY UN CRACK !!!

--- app/src/main/java/com/optic/nanima/RegisterActivity.java
+++ app/src/main/java/com/optic/nanima/RegisterActivity.java
@@ -1,13 +1,20 @@
 package com.optic.nanima;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +30,9 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText mTextInputConfirmPassword;
     Button mButtonRegister;
 
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +44,9 @@ public class RegisterActivity extends AppCompatActivity {
         mTextInputPassword = findViewById(R.id.textInputPassword);
         mTextInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
         mButtonRegister = findViewById(R.id.btnRegister);
+
+        // Initialize Firebase Auth ---------
+        mAuth = FirebaseAuth.getInstance();
 
         mButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +63,16 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+    }
+
     private void register() {
         String username = mTextInputUsername.getText().toString();
         String email = mTextInputEmail.getText().toString();
@@ -58,7 +81,33 @@ public class RegisterActivity extends AppCompatActivity {
 
         if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty()) {
             if (isEmailValid(email)) {
-                Toast.makeText(this, "Insertaste todos los campos y el email es valido", Toast.LENGTH_SHORT).show();
+                if (password.equals(confirmPassword)) {
+                    if (password.length() >= 6) {
+
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            FirebaseUser user = mAuth.getCurrentUser();
+                                            Toast.makeText(RegisterActivity.this, "LO HAS AÑADIDO BROO", Toast.LENGTH_SHORT).show();
+                                            //updateUI(user);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            //updateUI(null);
+                                        }
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(this, "Las contraseña no coinciden", Toast.LENGTH_SHORT).show();
+                }
             }
             else {
                 Toast.makeText(this, "Insertaste todos los campos pero el correo no es valido", Toast.LENGTH_LONG).show();
