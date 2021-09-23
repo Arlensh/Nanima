25fb459314de4d3ec6fa5f9a0aba77a8a66c5afc
Luciano <lucianobarrerapaez@gmail.com>
2021-09-23 13:19:52 +0200

    añadiendo un boton en el home para posteriormente hacer la págiana para los post de la aplicacion

---
+++ app/src/main/java/com/optic/nanima/activities/PostActivity.java
@@ -0,0 +1,17 @@
+package com.optic.nanima.activities;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.optic.nanima.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
