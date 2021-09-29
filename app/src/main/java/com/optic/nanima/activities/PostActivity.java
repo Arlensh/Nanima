60297ba07b606f4acdd5b81639890ff091d66c4c
Luciano <lucianobarrerapaez@gmail.com>
2021-09-29 20:19:10 +0200

    Hago que se pueda guardar cuando subo una foto a la base de datos de google storage

--- app/src/main/java/com/optic/nanima/activities/PostActivity.java
+++ app/src/main/java/com/optic/nanima/activities/PostActivity.java
@@ -1,17 +1,90 @@
 package com.optic.nanima.activities;
-
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.UploadTask;
 import com.optic.nanima.R;
+import com.optic.nanima.providers.ImageProvider;
+import com.optic.nanima.utils.FileUtil;
+
+import java.io.File;
 
 public class PostActivity extends AppCompatActivity {
+
+    ImageView mImageViewPost1;
+    File mImageFile;
+    Button mButtonPost;
+    ImageProvider mImageProvider;
+    private final int GALLERY_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mImageProvider = new ImageProvider();
+
+        mImageViewPost1 = findViewById(R.id.imageViewPost1);
+        mButtonPost = findViewById(R.id.btnPost);
+
+        mButtonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveImage();
+            }
+        });
+        mImageViewPost1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+    }
+
+    private void saveImage() {
+        mImageProvider.save(PostActivity.this, mImageFile).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(PostActivity.this, "La imagen se almaceno correctamente", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(PostActivity.this, "Hubo error al almacenar la imagen", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK) {
+            try {
+                mImageFile = FileUtil.from(this, data.getData());
+                mImageViewPost1.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+            } catch(Exception e) {
+                Log.d("ERROR", "Se produjo un error " + e.getMessage());
+                Toast.makeText(this, "Se produjo un error " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
