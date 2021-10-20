0352d67b50ff439d7568a0649afadf1c72a3a480
Luciano <lucianobarrerapaez@gmail.com>
2021-10-20 18:10:17 +0200

    Ya tengo hecho que se puedan crear comentarios y que se muestren en el post indicado, desde la base de datos de firebase.

--- app/src/main/java/com/optic/nanima/activities/PostDetailActivity.java
+++ app/src/main/java/com/optic/nanima/activities/PostDetailActivity.java
@@ -1,18 +1,38 @@
 package com.optic.nanima.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
 import com.optic.nanima.R;
+import com.optic.nanima.adapters.CommentAdapter;
 import com.optic.nanima.adapters.SliderAdapter;
+import com.optic.nanima.models.Comment;
 import com.optic.nanima.models.SliderItem;
+import com.optic.nanima.providers.AuthProvider;
+import com.optic.nanima.providers.CommentsProvider;
 import com.optic.nanima.providers.PostProvider;
 import com.optic.nanima.providers.UsersProvider;
 import com.smarteist.autoimageslider.IndicatorAnimations;
@@ -21,6 +41,7 @@ import com.smarteist.autoimageslider.SliderView;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -30,8 +51,13 @@ public class PostDetailActivity extends AppCompatActivity {
     SliderView mSliderView;
     SliderAdapter mSliderAdapter;
     List<SliderItem> mSliderItems = new ArrayList<>();
+
     PostProvider mPostProvider;
     UsersProvider mUsersProvider;
+    CommentsProvider mCommentsProvider;
+    AuthProvider mAuthProvider;
+
+    CommentAdapter mAdapter;
 
     String mExtraPostId;
 
@@ -43,6 +69,11 @@ public class PostDetailActivity extends AppCompatActivity {
     ImageView mImageViewCategory;
     CircleImageView mCircleImageViewProfile;
     Button mButtonShowProfile;
+    CircleImageView mCircleImageViewBack;
+    FloatingActionButton mFabComment;
+    RecyclerView mRecyclerView;
+
+    String mIdUser = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,13 +89,141 @@ public class PostDetailActivity extends AppCompatActivity {
         mImageViewCategory = findViewById(R.id.imageViewCategory);
         mCircleImageViewProfile = findViewById(R.id.circleImageProfile);
         mButtonShowProfile = findViewById(R.id.btnShowProfile);
+        mCircleImageViewBack = findViewById(R.id.circleImageBack);
+        mFabComment = findViewById(R.id.fabComment);
+        mRecyclerView = findViewById(R.id.recyclerViewComments);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(PostDetailActivity.this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
 
         mPostProvider = new PostProvider();
         mUsersProvider = new UsersProvider();
+        mCommentsProvider = new CommentsProvider();
+        mAuthProvider = new AuthProvider();
 
         mExtraPostId = getIntent().getStringExtra("id");
 
         getPost();
+
+        mFabComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogComment();
+            }
+        });
+
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        mButtonShowProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToShowProfile();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Query query = mCommentsProvider.getCommentsByPost(mExtraPostId);
+        FirestoreRecyclerOptions<Comment> options =
+                new FirestoreRecyclerOptions.Builder<Comment>()
+                        .setQuery(query, Comment.class)
+                        .build();
+        mAdapter = new CommentAdapter(options, PostDetailActivity.this);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAdapter.stopListening();
+    }
+
+    private void showDialogComment() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(PostDetailActivity.this);
+        alert.setTitle("¡COMENTARIO!");
+        alert.setMessage("Ingresa tu comentario");
+
+        final EditText editText = new EditText(PostDetailActivity.this);
+        editText.setHint("Texto");
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(36, 0, 36, 36);
+        editText.setLayoutParams(params);
+        RelativeLayout container = new RelativeLayout(PostDetailActivity.this);
+        RelativeLayout.LayoutParams relativeParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        container.setLayoutParams(relativeParams);
+        container.addView(editText);
+
+        alert.setView(container);
+
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String value = editText.getText().toString();
+                if (!value.isEmpty()) {
+                    createComment(value);
+                }
+                else {
+                    Toast.makeText(PostDetailActivity.this, "Debe ingresar el comentario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        alert.show();
+    }
+
+    private void createComment(String value) {
+        Comment comment = new Comment();
+        comment.setComment(value);
+        comment.setIdPost(mExtraPostId);
+        comment.setIdUser(mAuthProvider.getUid());
+        comment.setTimestamp(new Date().getTime());
+        mCommentsProvider.create(comment).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(PostDetailActivity.this, "El comentario se creo correctamente", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(PostDetailActivity.this, "No se pudo crear el comentario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void goToShowProfile() {
+        if (!mIdUser.equals("")) {
+            Intent intent = new Intent(PostDetailActivity.this, UserProfileActivity.class);
+            intent.putExtra("idUser", mIdUser);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "El id del usuario aun no se carga", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void instanceSlider() {
@@ -124,8 +283,8 @@ public class PostDetailActivity extends AppCompatActivity {
                         }
                     }
                     if (documentSnapshot.contains("idUser")) {
-                        String idUser = documentSnapshot.getString("idUser");
-                        getUserInfo(idUser);
+                        mIdUser = documentSnapshot.getString("idUser");
+                        getUserInfo(mIdUser);
                     }
 
                     instanceSlider();
